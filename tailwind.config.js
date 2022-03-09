4c0ee9775a06206bc9eb5d66450a62edd45b79fb
ayalamokhzanji <aya.almokhzanji@gmail.com>
2022-03-09 11:45:18 +0200

    feat install tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  content: [
+    './storage/framework/views/*.php',
+    './resources/**/*.blade.php',
+    './resources/**/*.js',
+    './resources/**/*.vue',
+    "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+],
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      'purple': '#3f3cbb',
+      'midnight': '#121063',
+      'metal': '#565584',
+      'tahiti': '#3ab7bf',
+      'silver': '#ecebff',
+      'bubble-gum': '#ff77e9',
+      'bermuda': '#78dcca',
+      'gray':{
+        50:'#f9fafb',
+        100: '#f3f4f6',
+        200: '#e5e7eb',
+        300: '#d1d5db',
+        400: '#9ca3af',
+        500: '#6b7280',
+        600: '#4b5563',
+        700: '#374151',
+        800: '#1f2937',
+        900: '#111827',
+      },
+    },
+  },
+  plugins: [],
+}
